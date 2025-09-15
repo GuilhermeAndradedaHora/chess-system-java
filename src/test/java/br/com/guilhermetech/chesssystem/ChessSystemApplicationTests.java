af4737993da6d213dd0474ec20f627222324bb8e
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-15 10:34:38 -0300

    Chess System

---
+++ src/test/java/br/com/guilhermetech/chesssystem/ChessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermetech.chesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
