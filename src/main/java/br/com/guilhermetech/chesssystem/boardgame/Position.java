715d08daf1476ec933fd28b620a9722cb5ef2727
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-15 10:42:26 -0300

    First Class: Position

---
+++ src/main/java/br/com/guilhermetech/chesssystem/boardgame/Position.java
@@ -0,0 +1,26 @@
+package br.com.guilhermetech.chesssystem.boardgame;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Position {
+
+    private Integer row;
+    private Integer column;
+
+    public Position() {
+    }
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
