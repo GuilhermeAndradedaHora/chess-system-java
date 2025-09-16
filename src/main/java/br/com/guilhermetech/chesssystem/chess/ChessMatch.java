060295e067efe39f2b060e9a0e84c80a9922e4c0
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-16 16:09:29 -0300

    Bishop

--- src/main/java/br/com/guilhermetech/chesssystem/chess/ChessMatch.java
+++ src/main/java/br/com/guilhermetech/chesssystem/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package br.com.guilhermetech.chesssystem.chess;
 import br.com.guilhermetech.chesssystem.boardgame.Board;
 import br.com.guilhermetech.chesssystem.boardgame.Piece;
 import br.com.guilhermetech.chesssystem.boardgame.Position;
+import br.com.guilhermetech.chesssystem.chess.pieces.Bishop;
 import br.com.guilhermetech.chesssystem.chess.pieces.King;
 import br.com.guilhermetech.chesssystem.chess.pieces.Pawn;
 import br.com.guilhermetech.chesssystem.chess.pieces.Rook;
@@ -186,7 +187,9 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -198,7 +201,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
